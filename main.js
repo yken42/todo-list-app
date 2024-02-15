3b0a838a710d04cf650972cd2dd2208bc5145f76
Yair Ken <yairkennn@gmail.com>
2024-02-15 20:28:09 +0200

    "reduced gap between lines"

--- main.js
+++ main.js
@@ -1,48 +1,30 @@
-const form = document.querySelector('form')
-let list = document.querySelector('.todo-list');
- let taskInput = document.querySelector(".task-input");
+const form = document.querySelector("form");
+let list = document.querySelector(".todo-list");
+let taskInput = document.querySelector(".task-input");
 
-
-
-function onFormSubmit(event){
-    event.preventDefault();
-    // on new task entered --> create new list item
-   createItems();
-    taskInput.value = '';
-   
+function onFormSubmit(event) {
+  event.preventDefault();
+  // on new task entered --> create new list item
+  createItems();
+  taskInput.value = "";
 }
 form.addEventListener("submit", onFormSubmit);
 
+function removeTask() {}
 
-function removeTask(){
-  
-}
- 
-
-function createItems(){
-    if(taskInput.value === ''){
-        alert('This space requires an input, can not leave it blank');
-    }
-    else{
-
-    
-   let newItem = document.createElement('li');
-   newItem.classList.add('item');
-   let itemText = taskInput.value;
-   newItem.innerHTML = `
+function createItems() {
+  if (taskInput.value === "") {
+    alert("This space requires an input, can not leave it blank");
+  } else {
+    let newItem = document.createElement("li");
+    newItem.classList.add("item");
+    let itemText = taskInput.value;
+    newItem.innerHTML = `
    <input type="checkbox" class="unchecked" />
    <label for="task">${itemText}</label>
-   <span>X</span>`
-   list.appendChild(newItem);
+   <span>X</span>`;
+    list.appendChild(newItem);
+  }
 }
 
-
-
-
-   
-
-}
-
-function init(){
-   
-}
+function init() {}
